c2557255d247367f271f9293988ac0cf1c10ecf9
hugo <hugo.monmousseau@hotmail.fr>
2022-09-07 11:21:54 +0200

    AiShapeCircle

--- Assets/Scripts/ecranTactile/AILineShapeDetection.cs
+++ Assets/Scripts/ecranTactile/AILineShapeDetection.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AILineShapeDetection : MonoBehaviour
 {
-    public string shape = "";
+    public ShapeType shape = ShapeType.None;
     private LineRenderer line;
     [SerializeField] private List<Vector2> positions;
     [SerializeField] private List<float> coef;
@@ -13,7 +13,23 @@ public class AILineShapeDetection : MonoBehaviour
     //pour x seulement
     private float moyenne;
     private float total;
+
+    [SerializeField] private float lineLength;
+    [SerializeField] private float supposeLineLength;
     [SerializeField] private List<float> positionsX;
+
+    [SerializeField] private List<float> positionsY;
+    [SerializeField] private List<Vector2> cardialsPoints;
+    [SerializeField] private List<float> cardinalsLength;
+    [SerializeField] private List<float> cardinalsLengthMaths;
+    [SerializeField] private float yHigh;
+    [SerializeField] private float yLow;
+    [SerializeField] private float xHigh;
+    [SerializeField] private float xLow;
+    [SerializeField] private List<Vector2> yHighList;
+    [SerializeField] private List<Vector2> yLowList;
+    [SerializeField] private List<Vector2> xHighList;
+    [SerializeField] private List<Vector2> xLowList;
     private void Start()
     {
         line = GetComponent<LineRenderer>();
@@ -26,6 +42,7 @@ public class AILineShapeDetection : MonoBehaviour
             positions.Add(line.GetPosition(i));
             total += line.GetPosition(i).x;
             positionsX.Add(positions[i].x);
+            positionsY.Add(positions[i].y);
         }
         moyenne = total / line.positionCount;
         //est ce un trait 
@@ -37,25 +54,75 @@ public class AILineShapeDetection : MonoBehaviour
                 //sont ils plus ou moins alignés
                 coef.Add((positions[i].y - positions[0].y) / (positions[i].x - positions[0].x));
             }
-            //on récup la différence des 2 coef les plus eloignés           
-
+            //"perimetre"         
+            lineLength += Mathf.Sqrt(Mathf.Pow(positions[i].x - positions[i - 1].x, 2) + Mathf.Pow(positions[i].y - positions[i - 1].y, 2));
         }
         coef.Sort();//tri
         positionsX.Sort();
+        positionsY.Sort();
         tolerence += (tolerence / 25)*line.positionCount;
         //si la droite n est pas verticale
         if (Mathf.Sqrt((coef[0] - coef[coef.Count - 1]) * (coef[0] - coef[coef.Count - 1])) < tolerence)//valeur absolue
         {
-            Debug.Log("trait");
-            Debug.Log(Mathf.Sqrt((coef[0] - coef[coef.Count - 1]) * (coef[0] - coef[coef.Count - 1])));
+            shape = ShapeType.Line;
         }
         //si elle est verticale
         else if (moyenne - tolerence/2 < positionsX[0] && moyenne + tolerence/2 > positionsX[positionsX.Count - 1] && ValeurAbsolue(startShape.y - endShape.y) > tolerence)
         {
-            Debug.Log("trait v");
-            Debug.Log(moyenne + tolerence / 2);
-            Debug.Log(moyenne - tolerence / 2);
+            shape = ShapeType.Line;
         }
+
+        if(shape != ShapeType.Line)
+        {
+            yHigh = positionsY[positionsY.Count - 1];
+            yLow = positionsY[0];
+            xHigh = positionsX[positionsX.Count - 1];
+            xLow = positionsX[0];
+            for (int i = 0; i < positions.Count; i++)
+            {
+                if (positions[i].y == positionsY[positionsY.Count - 1])
+                {
+                    yHighList.Add(positions[i]);
+                    
+                }
+                if (positions[i].y == positionsY[0])
+                {
+                    yLowList.Add(positions[i]);
+                }
+                if (positions[i].x == positionsX[positionsX.Count - 1])
+                {
+                    xHighList.Add(positions[i]);
+                }
+                if (positions[i].x == positionsX[0])
+                {
+                    xLowList.Add(positions[i]);
+                }
+
+            }
+            cardialsPoints.Add(new Vector2(yHighList[(int)yHighList.Count / 2].x, yHigh));
+            cardialsPoints.Add(new Vector2(xHigh, xHighList[(int)xHighList.Count / 2].y));
+
+            cardialsPoints.Add(new Vector2(yLowList[(int)yLowList.Count / 2].x, yLow));
+            cardialsPoints.Add(new Vector2(xLow, xLowList[(int)xLowList.Count / 2].y));
+
+            for (int i = 1; i < 4; i++)
+            {
+                cardinalsLength.Add(Mathf.Sqrt(Mathf.Pow(cardialsPoints[i].x - cardialsPoints[i - 1].x, 2) + Mathf.Pow(cardialsPoints[i].y - cardialsPoints[i - 1].y, 2)));
+            }
+            cardinalsLength.Add(Mathf.Sqrt(Mathf.Pow(cardialsPoints[3].x - cardialsPoints[0].x, 2) + Mathf.Pow(cardialsPoints[3].y - cardialsPoints[0].y, 2)));
+            for (int i = 0; i < cardialsPoints.Count; i++)
+            {
+                cardinalsLengthMaths.Add((cardinalsLength[i]*Mathf.PI/2));
+                supposeLineLength += cardinalsLengthMaths[i];
+            }
+            supposeLineLength -= tolerence;
+            if(ValeurAbsolue(supposeLineLength - lineLength) < tolerence)
+            {
+                shape = ShapeType.Circle;
+            }
+        }
+
+
     }
     private float ValeurAbsolue(float nb)
     {
@@ -63,4 +130,11 @@ public class AILineShapeDetection : MonoBehaviour
     }
 
 
+}
+
+public enum ShapeType
+{
+    None,
+    Circle,
+    Line,
 }
