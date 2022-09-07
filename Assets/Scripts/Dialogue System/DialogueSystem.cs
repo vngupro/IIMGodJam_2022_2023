8d05c0d70c435a2b28e4c417f30644a997edeb1e
Victor Lacombe <lol.lacombe@gmail.com>
2022-09-07 14:40:43 +0200

    Dialogue System Done

--- Assets/Scripts/Dialogue System/DialogueSystem.cs
+++ Assets/Scripts/Dialogue System/DialogueSystem.cs
@@ -1,27 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshPro))]
 
 public class DialogueSystem : MonoBehaviour
 {
     public string[] sentences;
-    public float[] waitTime;
-    public bool[] isOnClick;
-    private bool isFinished = false;
+    private TextMeshPro text;
+    private bool Trigger = false;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        text = GetComponent <TextMeshPro>();
+    }
 
+    private bool DoesPlayerSkip()
+    {
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    private void Update()
+    {
+        if(Trigger)
+        {
+            if(Input.GetKeyDown(KeyCode.M))
+            {
+                StartCoroutine(Talking());
+                Trigger = false;
+            }
+        }
+    }
+
+    IEnumerator Talking()
+    {
+        foreach (string item in sentences)
+        {
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.M));
+            text.text = item;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player") && !isFinished)
+        if(collision.CompareTag("Player"))
         {
-            
+            Trigger = true;
         }
     }
-
-
 }
